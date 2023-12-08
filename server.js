dc0855022aa7e4b17e36de5db15541353e902dcd
pittman.anthony17@gmail.com <pittman.anthony17@gmail.com>
2023-12-07 17:55:13 -0800

    mongoose

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 const methodOverride = require('method-override')
 const express = require('express')
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+
 
 // CONFIGURATION
 require('dotenv').config()
