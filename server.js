1b15404d91abb1df12f01917e38c66b2587e9342
pittman.anthony17@gmail.com <pittman.anthony17@gmail.com>
2023-12-14 22:16:10 -0800

    ok

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 // DEPENDENCIES
 const methodOverride = require('method-override')
 const express = require('express')
-
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-)
+
 
 
 // CONFIGURATION
